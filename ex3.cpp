30bff7336a79cf93dc2e10c8605c0e064b65ed02
Egor Lisenkov <145376225+erlisenkov@users.noreply.github.com>
2023-12-25 11:59:02 +0300

    Update ex3.cpp

--- ex3.cpp
+++ ex3.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
 #include <queue>
 using namespace std;
+
+// Класс для нахождения медианы из потока чисел
 class MedianFinder {
 public:
-    priority_queue<int> decreasing;
-    priority_queue<int, std::vector<int>, std::greater<int>> increasing;
-    bool evn = true;
+    priority_queue<int> decreasing;  // Очередь с приоритетом для убывающих значений
+    priority_queue<int, std::vector<int>, std::greater<int>> increasing;  // Очередь с приоритетом для возрастающих значений
+    bool even = true;  // Флаг для отслеживания четности/нечетности текущего размера потока
 
+    // Конструктор класса
     MedianFinder() {
     }
 
+    // Метод для добавления числа в поток
     void addNum(int num) {
-        evn =!evn;
-        if(!evn){
+        even = !even;  // Переключение флага четности/нечетности
+
+        // Если размер потока нечетный
+        if (!even) {
             decreasing.push(num);
             increasing.push(decreasing.top());
             decreasing.pop();
-        }else{
+        } else {  // Если размер потока четный
             increasing.push(num);
             decreasing.push(increasing.top());
             increasing.pop();
         }
     }
 
+    // Метод для нахождения медианы
     double findMedian() {
-        if (!evn) return increasing.top();
+        // Если размер потока нечетный, возвращаем верхний элемент из очереди с приоритетом для возрастающих значений
+        if (!even) return increasing.top();
+        
+        // Если размер потока четный, возвращаем среднее значение двух верхних элементов
         return (decreasing.top() + increasing.top()) / 2.0;
     }
 };
 
+// Точка входа в программу
 int main() {
+    // Создание объекта класса MedianFinder
     MedianFinder mf;
+
+    // Добавление чисел в поток и вывод медианы
     mf.addNum(1);
     mf.addNum(2);
     cout << mf.findMedian() << std::endl;
     mf.addNum(3);
     cout << mf.findMedian() << std::endl;
+
     return 0;
 }
